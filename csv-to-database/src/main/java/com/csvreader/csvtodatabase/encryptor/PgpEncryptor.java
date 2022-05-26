65b407644cd6bf5e78b392cd90160a3df4c8decd
ParamMalik <86033103+ParamMalik@users.noreply.github.com>
2022-05-26 15:05:35 +0530

    updated

--- csv-to-database/src/main/java/com/csvreader/csvtodatabase/encryptor/PgpEncryptor.java
+++ csv-to-database/src/main/java/com/csvreader/csvtodatabase/encryptor/PgpEncryptor.java
@@ -54,7 +54,7 @@ public class PgpEncryptor {
         throw new IllegalArgumentException("Can't find encryption key in key ring.");
     }
 
-    private ByteArrayInputStream encryptFile(PGPPublicKey encryptionPGPPublicKey, byte[] bytesToEncrypt) throws IOException {
+    private ByteArrayInputStream encryptFile(PGPPublicKey encryptionPGPPublicKey, byte[] bytesToEncrypt) {
 
         var byteArrayOutputStream = new ByteArrayOutputStream();
         OutputStream outputStream = byteArrayOutputStream;
@@ -70,40 +70,52 @@ public class PgpEncryptor {
             openStream.write(arrayOfByte);
 
         } catch (Exception localPGPException) {
-            log.error("PGP Error occurred:{}",localPGPException.getMessage());
-            return null;
+            log.error("PGP Error occurred:{}", localPGPException.getMessage());
 
         } finally {
-            assert openStream != null;
-            openStream.close();
-            outputStream.close();
+            try {
+                assert openStream != null;
+                openStream.close();
+                outputStream.close();
+            } catch (IOException e) {
+                System.out.println("Error while closing the stream" + e.getMessage());
+            }
+
         }
 
         var encryptedDataAsString = byteArrayOutputStream.toString();
-
+        System.out.println(encryptedDataAsString);
         return new ByteArrayInputStream(encryptedDataAsString.getBytes());
     }
 
 
     // Compressing
-    private static byte[] compress(byte[] byteDataToEncrypt) throws IOException {
+    private static byte[] compress(byte[] byteDataToEncrypt) {
 
         var byteArrayOutputStream = new ByteArrayOutputStream();
         var compressedData = new PGPCompressedDataGenerator(CompressionAlgorithmTags.ZIP);
-        var compressedOutputStream = compressedData.open(byteArrayOutputStream);
-        var localPGPCompressedDataGenerator = new PGPLiteralDataGenerator();
-        var outputStream = localPGPCompressedDataGenerator.open(compressedOutputStream, // the compressed output stream
-                PGPLiteralData.BINARY,
-                "",
-                byteDataToEncrypt.length,
-                new Date()
-        );
-
-        outputStream.write(byteDataToEncrypt);
-
-        outputStream.close();
-        compressedData.close();
-
+        OutputStream outputStream = null;
+        try {
+            var compressedOutputStream = compressedData.open(byteArrayOutputStream);
+            var localPGPCompressedDataGenerator = new PGPLiteralDataGenerator();
+            outputStream = localPGPCompressedDataGenerator.open(compressedOutputStream, // the compressed output stream
+                    PGPLiteralData.BINARY,
+                    "",
+                    byteDataToEncrypt.length,
+                    new Date()
+            );
+            outputStream.write(byteDataToEncrypt);
+        } catch (Exception exception) {
+            log.error("Error while compressing the data " + exception.getMessage());
+        } finally {
+            try {
+                assert outputStream != null;
+                outputStream.close();
+                compressedData.close();
+            } catch (IOException e) {
+                log.error("Error While closing the stream" + e.getMessage());
+            }
+        }
         return byteArrayOutputStream.toByteArray();
     }
 
